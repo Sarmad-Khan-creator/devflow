f3d788432802fc24e7b8ecbca09655f8f3eb8c96
Sarmad-Khan-creator <sarmadkhan2694@gmail.com>
2023-12-17 22:24:08 +0500

    filters and question cards

---
+++ components/home/HomeFilters.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { HomePageFilters } from "@/constants/filters";
+import React, { useState } from "react";
+import { Button } from "../ui/button";
+import { useRouter, useSearchParams } from "next/navigation";
+
+const HomeFilters = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const [active, setActive] = useState("");
+
+  return (
+    <div className="mt-10 hidden flex-wrap gap-3 md:flex">
+      {HomePageFilters.map((item) => (
+        <Button
+          key={item.value}
+          onClick={() => {}}
+          className={`body-me dium rounded-lg px-6 py-3 capitalize shadow-none ${
+            active === item.value
+              ? "dark:hover:bg-dark400 bg-primary-100 text-primary-500 hover:bg-primary-100 dark:bg-dark-400 dark:text-primary-500"
+              : "bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300"
+          }`}
+          onClickCapture={() => {}}
+        >
+          {item.name}
+        </Button>
+      ))}
+    </div>
+  );
+};
+
+export default HomeFilters;
