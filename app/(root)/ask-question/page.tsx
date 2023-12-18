a1fddd28b404de70b74269ebe6f475f2c3892bf5
Sarmad-Khan-creator <sarmadkhan2694@gmail.com>
2023-12-18 17:45:54 +0500

    forms

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,18 @@
+import Question from "@/components/forms/Question";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const AskQuestion = () => {
-  return <div>AskQuestion</div>;
+const Page = async () => {
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 };
 
-export default AskQuestion;
+export default Page;
