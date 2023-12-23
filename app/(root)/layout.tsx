dda1b3ad639f5347347743328660da84683f0062
Sarmad-Khan-creator <sarmadkhan2694@gmail.com>
2023-12-23 18:37:56 +0500

    updated auth layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import RightSidebar from "@/components/shared/RightSidebar";
 import Navbar from "@/components/shared/navbar/Navbar";
 import React from "react";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="background-light850_dark100 relative">
       <Navbar />
@@ -16,6 +16,4 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       </div>
     </main>
   );
-};
-
-export default Layout;
+}
