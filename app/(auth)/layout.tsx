6ee13a3517b578f917565aaf8a02f2c3dbc0a8c1
Sarmad-Khan-creator <sarmadkhan2694@gmail.com>
2023-12-23 21:25:45 +0500

    commit changes of layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,9 @@
-import { LayoutProps } from "@/.next/types/app/layout";
 import React from "react";
 
-const Layout = (props: LayoutProps) => {
+const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <main className="flex min-h-screen w-full items-center justify-center">
-      {props.children}
+      {children}
     </main>
   );
 };
