acdecaacd0b542d1b740599a7e48f7766dad5bd8
Sarmad-Khan-creator <sarmadkhan2694@gmail.com>
2024-01-06 19:33:35 +0500

    ...

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -70,10 +70,13 @@ const Answer = ({ question, questionId, authorId }: Props) => {
     setIsSubmittingAI(true);
 
     try {
-      const response = await fetch(`http://localhost:3000/api/chatgpt`, {
-        method: "POST",
-        body: JSON.stringify({ question }),
-      });
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/chatgpt`,
+        {
+          method: "POST",
+          body: JSON.stringify({ question }),
+        }
+      );
 
       const aiAnswer = await response.json();
 
